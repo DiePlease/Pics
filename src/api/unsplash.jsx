04875fdd1803023dcd3dd4b2cec296876b673ede
Naveen Singh Adhikari <meetnsa1@gmail.com>
2020-10-08 08:50:16 +0530

    Update unsplash.jsx

--- src/api/unsplash.jsx
+++ src/api/unsplash.jsx
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID e3964ac1bb8682f404764674ef5365f7b0ea74f2f5272ddb71150be2b3777754"
+      
   }
 });
